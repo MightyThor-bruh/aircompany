2da84c5446dcba49e13236634055b278b4ab5642
gaechka <liza.schumova@gmail.com>
2022-11-12 16:29:01 +0300

    fixed this

--- Net/Aircompany/Planes/PassengerPlane.cs
+++ Net/Aircompany/Planes/PassengerPlane.cs
@@ -4,18 +4,17 @@ namespace Aircompany.Planes
 {
     public class PassengerPlane : Plane
     {
-        public int _passengersCapacity;
+        public readonly int _passengersCapacity; //readonly
 
         public PassengerPlane(string model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, int passengersCapacity)
-            :base(model, maxSpeed, maxFlightDistance, maxLoadCapacity)
+            : base(model, maxSpeed, maxFlightDistance, maxLoadCapacity)
         {
             _passengersCapacity = passengersCapacity;
         }
 
         public override bool Equals(object obj)
         {
-            var plane = obj as PassengerPlane;
-            return plane != null &&
+            return obj is PassengerPlane plane && //as =>is
                    base.Equals(obj) &&
                    _passengersCapacity == plane._passengersCapacity;
         }
@@ -33,13 +32,11 @@ namespace Aircompany.Planes
             return _passengersCapacity;
         }
 
-       
+
         public override string ToString()
         {
-            return base.ToString().Replace("}",
-                    ", passengersCapacity=" + _passengersCapacity +
-                    '}');
-        }       
-        
+            return base.ToString().Replace("}", ", passengersCapacity=" + _passengersCapacity + '}');
+        }
+
     }
 }
